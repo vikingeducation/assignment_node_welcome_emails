2ef24b832a866d8f6ef635519897bcb1dcc5686b
nicoasp <nicolas.amaya@gmail.com>
2017-05-22 19:03:11 +0200

    set up, routes, views

---
+++ app.js
@@ -0,0 +1,179 @@
+const express = require('express');
+const app = express();
+
+
+// ----------------------------------------
+// ENV
+// ----------------------------------------
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+// ----------------------------------------
+// Site Config
+// ----------------------------------------
+app.use((req, res, next) => {
+  res.locals.siteTitle = 'Nodemailer Assignment';
+  next();
+});
+
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// ----------------------------------------
+// Flash Messages
+// ----------------------------------------
+const flash = require('express-flash-messages');
+app.use(flash());
+
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require('morgan');
+const highlight = require('cli-highlight').highlight;
+
+// Add :data format token
+// to `tiny` format
+let format = [
+  ':separator',
+  ':newline',
+  ':method ',
+  ':url ',
+  ':status ',
+  ':res[content-length] ',
+  '- :response-time ms',
+  ':newline', ':newline',
+  ':data',
+  ':newline',
+  ':separator',
+  ':newline', ':newline',
+].join('');
+
+// Use morgan middleware with
+// custom format
+app.use(morgan(format));
+
+// Helper tokens
+morgan.token('separator', () => '****');
+morgan.token('newline', () => "\n");
+
+// Set data token to output
+// req query params and body
+morgan.token('data', (req, res, next) => {
+  let data = [];
+  ['query', 'params', 'body', 'session'].forEach((key) => {
+    if (req[key]) {
+      let capKey = key[0].toUpperCase() + key.substr(1);
+      let value = JSON.stringify(req[key], null, 2);
+      data.push(`${ capKey }: ${ value }`);
+    }
+  });
+  data = highlight(data.join('\n'), {
+    language: 'json',
+    ignoreIllegals: true
+  });
+  return `${ data }`;
+});
+
+
+// ----------------------------------------
+// Template Engine
+// ----------------------------------------
+const expressHandlebars = require('express-handlebars');
+const flashHelper = require('./helpers/flash');
+
+
+const hbs = expressHandlebars.create({
+  helpers: flashHelper,
+  partialsDir: 'views',
+  defaultLayout: 'main'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// ----------------------------------------
+// Routes
+// ----------------------------------------
+const EmailService = require('./services/email');
+
+
+app.get('/', (req, res) => {
+  res.render('users/new');
+});
+
+
+app.post('/users', (req, res, next) => {
+  const text = "Hey, welcome aboard " 
+  + req.body.first_name + " " 
+  + req.body.last_name + " "
+  + "Your registration data: \n" 
+  + "Email: " + req.body.email + "\n"
+  + "Password" + req.body.password;
+
+  const options = {
+    from: process.env.EMAIL_USER,
+    to: req.body.email,
+    subject: "Registration successful",
+    text,
+    html: `<p>${ text }</p>`
+  };
+
+  EmailService.send(options)
+    .then((result) => {
+      result = JSON.stringify(result, null, 2)
+      req.flash('success', 'Registered!');
+      res.render('home', { result });
+    })
+    .catch(next);
+});
+
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+const port = process.env.PORT ||
+  process.argv[2] ||
+  3000;
+const host = 'localhost';
+
+
+let args;
+process.env.NODE_ENV === 'production' ?
+  args = [port] :
+  args = [port, host];
+
+args.push(() => {
+  console.log(`Listening: http://${ host }:${ port }\n`);
+});
+
+
+app.listen.apply(app, args);
+
+
+// ----------------------------------------
+// Error Handling
+// ----------------------------------------
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err.stack) {
+    err = err.stack;
+  }
+  res.status(500).render('errors/500', { error: err });
+});
+
+
+module.exports = app;
+
