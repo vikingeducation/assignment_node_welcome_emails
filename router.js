e99d21e9c37eb3255ae3812b9bf25ba11700cc98
Stephanie Barker <song_mage@yahoo.com>
2017-09-12 12:15:58 -0400

    it works

--- router.js
+++ router.js
@@ -1,11 +1,34 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => res.render("register"));
+const EmailService = require("./services/email");
+
+router.get("/", (req, res) =>
+	res.render("register", { status: req.session.status })
+);
 
 router.post("/register", (req, res) => {
-  const { email, fname, lname, password } = req.body;
-  // send email...
-  res.redirect("/");
+	const { email, fname, lname, password } = req.body;
+	const message = `We're glad to have you aboard, ${fname} ${lname}! By the way, your password is ${password}. We are a very secure site here at UselessApp.`;
+
+	const options = {
+		from: process.env.EMAIL_USER,
+		to: email,
+		subject: "Welcome to UselessApp!",
+		text: message,
+		html: `<p>${message}</p>`
+	};
+
+	EmailService.send(options)
+		.then(result => {
+			req.session.status = result;
+			req.flash("success", "Sent!");
+			res.redirect("/");
+		})
+		.catch(e => {
+			req.flash("danger", "There was an error :(");
+			req.session.status = e.stack;
+			res.redirect("/");
+		});
 });
 
 module.exports = router;
