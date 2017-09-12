fb18172fafe7b04d40880fae50af14561ccf9f6e
Andrew Senner <me@andrewsenner.com>
2017-09-12 12:10:36 -0400

    done with 1st assignment

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const registerRouter = require("/routes/registerEmail");
+const registerRouter = require('./routes/registerEmail');
 
 //bodyParser
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/public`));
 
-const flash = require("express-flash-messages");
+const flash = require('express-flash-messages');
 app.use(flash());
 
 //views
-const expressHandlebars = require("express-handlebars");
+const expressHandlebars = require('express-handlebars');
 const hbs = expressHandlebars.create({
-  partialsDir: "views",
-  defaultLayout: "application"
+	partialsDir: 'views',
+	defaultLayout: 'application'
 });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //ROUTES
-app.use("/register", registerRouter);
+app.use('/register', registerRouter);
 
 const port = process.env.PORT || process.argv[2] || 3000;
-const host = "localhost";
+const host = 'localhost';
 
 let args;
-process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+process.env.NODE_ENV === 'production' ? (args = [port]) : (args = [port, host]);
 
 args.push(() => {
-  console.log(`Listening: http://${host}:${port}\n`);
+	console.log(`Listening: http://${host}:${port}\n`);
 });
 
 app.listen.apply(app, args);
--- routes/registerEmail.js
+++ routes/registerEmail.js
@@ -1,16 +1,59 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
+const bluebird = require('bluebird');
+const Promise = bluebird;
 
-router.get("/", (req, res) => {
-  res.render("register");
+require('dotenv').config();
+
+router.get('/', (req, res) => {
+	res.render('register');
 });
-router.post("/", (req, res) => {
-  //grab our form data
-  const formData = {
-    firstName: req.body.first_name,
-    lastName: req.body.last_name,
-    email: req.body.email,
-    password: req.body.password
-  };
-  //send off to email services
+router.post('/', async (req, res) => {
+	//grab our form data
+	const formData = {
+		firstName: req.body.first_name,
+		lastName: req.body.last_name,
+		email: req.body.email,
+		password: req.body.password
+	};
+	//send off to email services
+	//// Use Heroku's SendGrid ENV variables
+	const _options = {
+		service: 'SendGrid',
+		auth: {
+			api_user: process.env.SENDGRID_USERNAME,
+			api_key: process.env.SENDGRID_PASSWORD
+		}
+	};
+
+	// Create the SendGrid transport
+	const _transporter = nodemailer.createTransport(sendGridTransport(_options));
+	_transporter.sendMail = bluebird.promisify(_transporter.sendMail);
+
+	console.log(JSON.stringify(formData, null, 2));
+
+	let text = `Merry Christmas, ya filthy animal, and a Happy new year. ${formData.firstName} ${formData.lastName} - Welcome to the club bruh!`;
+	text = text.concat(JSON.stringify(formData, null, 2));
+
+	const email = {
+		to: [
+			formData.email,
+			'drewdiddy611@gmail.com',
+			'henrydontshowmeyourlegs@gmail.com'
+		],
+		from: 'no-reply@email-sending-thing-a-ma-jigger.com',
+		subject: `I'm craaaaaazzzy`,
+		text: text
+	};
+
+	try {
+		const res = await _transporter.sendMail(email);
+		console.log(res);
+	} catch (err) {
+		console.err(err, err.stack);
+	}
 });
+
+module.exports = router;
